28658fb700e5488be62d4a799b50975d2ac32761
SaurabhPatil1112 <snp44v@gmail.com>
2022-08-19 20:46:47 +0530

    tensorflow test

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
 from flask import Flask, render_template
-
+import tensorflow
+import keras
+import cv2
 app = Flask(__name__)
 
 
 @app.route("/")
 def hello_world():
+    model = keras.models.load_model("hcr_related\hcr3.h5", compile=False)
+    img = cv2.imread("hcr_related\s.png", cv2.IMREAD_GRAYSCALE)
+    img = img.reshape(784)
+    prediction = model.predict(img)
+    print(prediction)
     return render_template("index.html")
 
 
